9580735d0612d5449d4dce26fb9090574df68810
Nitithon Kobtham <nitithon.gett@gmail.com>
2021-10-01 15:56:37 +0700

    fix

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../index')
 
-describe('GET /users', () => {
+describe('GET /', () => {
     it('responds with "Hello world!"', (done) => {
         request(app).get('/').expect('Hello world!', done)
     })
